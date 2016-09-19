d04aa5c4a70ad1eacac4b0518093084c75d9e775
Chris <orthagonal1@gmail.com>
2016-09-19 14:42:40 -0500

    working CLI version
    -uses AWS profiles and env variables
    -crops
    -compresses
    -uploads and prints s3 result

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+'use strict';
 const s3 = require('./lib/s3.js');
 const image = require('./lib/image.js');
 const _ = require('lodash');
 const aws_auth = require('./lib/commonAWS');
 const async = require('async');
+const path = require('path');
+const fs = require('fs');
 
 const argv = require('yargs')
 .options({
@@ -20,17 +23,21 @@ const argv = require('yargs')
   },
   'position': {
     describe: 'used in cropping, indicates the x,y coord for the upper-left corner of the cropping square',
+    nargs: 2,
     default: [0,0]
   },
   'size': {
     describe: 'used in cropping, indicates the length of the x,y axes for the cropping square',
-    default: [0,0]
+    nargs: 2,
+    default: false
   },
 })
+.demand(1)
 .argv;
 
 // establish AWS credentials:
 const aws = aws_auth(argv);
+const fileName = _.last(argv._);
 
 // do the main pipeline:
 async.auto({
@@ -40,25 +47,26 @@ async.auto({
         return done(err, result);
       });
     }
-    return done(null, fileName);
+    // if no compress, just copy file to /build:
+    const destName = path.join('build', fileName);
+    fs.mkdirSync('build');
+    fs.createReadStream(fileName).pipe(fs.createWriteStream(destName));
+    return done(null, destName);
   },
   crop: ['compress', (results, done) => {
     if (argv.size) {
-      return image.crop(results.compress, argv.size, argv.position, (err, result) => {
-        return done();
-      })
+      return image.crop(results.compress, argv.position, argv.size, (err, result) => {
+        return done(err, result);
+      });
     }
     return done(null, results.compress);
   }],
   upload: ['crop', (results, done) => {
-    return s3.upload(aws, argv.bucket, results.crop)
-    console.log('uploading')
-    return done();
+    return s3.put(aws, argv.bucket, results.crop, done);
   }]
 }, (err, results) => {
-  console.log('all done!')
   if (err) {
     console.log(err);
   }
-  console.log(results)
+  console.log(results.upload)
 });
