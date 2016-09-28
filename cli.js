78488e25d2f0a6228dec0bd80cf691dccae088d0
orthagonal <orthagonal1@gmail.com>
2016-09-28 14:54:17 -0500

    V1 (#1)
    * first commit, needs to be tidied before PR/code review
    
    * working CLI version
    
    -uses AWS profiles and env variables
    -crops
    -compresses
    -uploads and prints s3 result
    
    * lib + cli version
    
    -lint not firing in my editor yet, will need lint fixes
    
    * lint fixes
    
    --added lintrc
    
    * use os.tmpdir() instead of local 'build/' dir
    
    will also now demand a --bucket option when run from cli
    
    * uses npm's main and bin specifiers for lib/cli use
    
    "s3put" should run the executable once installed
    should load main.js when used as a module
    
    * added and fixed unit tests
    
    --updated to work correctly with file streams
    --handles generating the tmp file and cleaning it up in a more accurate
    fashion
    --unit tests for each of the basic combinations of operations
    --tests use env vars instead of hard-coding AWS vars
    --added missing packages
    
    * add --imagemagick flag

---
+++ cli.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+const main = require('./main.js');
+const _ = require('lodash');
+
+const argv = require('yargs')
+.options({
+  profile: {
+    describe: 'name of aws profile to use',
+    default: false
+  },
+  bucket: {
+    describe: 's3 bucket to upload the image to',
+    demand: true
+  },
+  quality: {
+    describe: 'set to a number between 0 and 100 to compress the image',
+    default: false
+  },
+  position: {
+    describe: 'used in cropping, indicates the x,y coord for the upper-left corner of the cropping square',
+    nargs: 2,
+    default: [0, 0]
+  },
+  size: {
+    describe: 'used in cropping, indicates the length of the x,y axes for the cropping square',
+    nargs: 2,
+    default: false
+  },
+  imagemagick: {
+    describe: 'by default will use GraphicsMagick, set to true to use the ImageMagick binaries',
+    default: false
+  }
+})
+.demand(1)
+.demand('bucket')
+.argv;
+
+const fileName = _.last(argv._);
+main(fileName, argv, (err, results) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(results);
+});
---
+++ main.js
@@ -0,0 +1,65 @@
+'use strict';
+const s3 = require('./lib/s3.js');
+const image = require('./lib/image.js');
+const awsAuth = require('./lib/commonAWS');
+const async = require('async');
+const path = require('path');
+const fs = require('fs-extra');
+const os = require('os');
+
+const execute = (imageFilePath, argv, callback) => {
+  // establish AWS credentials:
+  const aws = awsAuth(argv);
+  // const tmpFilePath = path.join(os.tmpdir(), path.basename(imageFilePath));
+  // do the main pipeline:
+  async.auto({
+    compress: (done) => {
+      if (argv.quality) {
+        return image.compress(imageFilePath, argv.quality, (err, result) => {
+          return done(err, result);
+        });
+      }
+      return done(null, imageFilePath);
+    },
+    crop: ['compress', (results, done) => {
+      if (argv.size) {
+        return image.crop(argv.imagemagick, results.compress, argv.position, argv.size, (err, result) => {
+          return done(err, result);
+        });
+      }
+      return done(null, results.compress);
+    }],
+    upload: ['crop', (results, done) => {
+      return s3.put(aws, argv.bucket, results.crop, done);
+    }],
+  }, (err, results) => {
+    callback(err, results.upload);
+  });
+};
+
+module.exports = (stream, options, callback) => {
+  const originalFilePath = typeof stream === 'string' ? stream : stream.path;
+  const tmpFilePath = path.join(os.tmpdir(), path.basename(originalFilePath));
+  const originalCallback = callback;
+  // wrap the callback so it cleans up and returns the results:
+  callback = (err, uploadResult) => {
+    fs.remove(tmpFilePath);
+    return originalCallback(err, uploadResult);
+  };
+  // make sure options is set
+  if (options.profile === undefined) {
+    options.profile = false;
+  }
+  // make sure the tmp file is written into the tmp folder:
+  if (typeof stream === 'object' && stream.path) {
+    const writeStream = fs.createWriteStream(tmpFilePath);
+    writeStream.on('finish', () => {
+      execute(tmpFilePath, options, callback);
+    });
+    stream.pipe(writeStream);
+  } else {
+    // just copy the file to the temp folder:
+    fs.copySync(originalFilePath, tmpFilePath);
+    execute(tmpFilePath, options, callback);
+  }
+};
