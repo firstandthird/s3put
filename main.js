bedc9dfc57b0963d16dc521748d77d60cba0041e
Chris <orthagonal1@gmail.com>
2017-12-22 01:03:57 -0600

    update to hapi 17, remove async/mocha/chai

--- main.js
+++ main.js
@@ -1,79 +1,46 @@
 'use strict';
 const awsAuth = require('aws-creds');
 const AWS = require('aws-sdk');
-const async = require('async');
 const path = require('path');
 const mimeLookup = require('mime-types').lookup;
 const datefmt = require('datefmt');
 const fs = require('fs');
 const url = require('url');
-
-module.exports = (input, options, allDone) => {
+const util = require('util');
+module.exports = async(input, options) => {
   if (!options.bucket) {
-    return allDone(new Error('bucket must be passed in'));
+    throw new Error('bucket must be passed in');
   }
-  async.autoInject({
-    aws(done) {
-      const aws = awsAuth(AWS, 'S3', options);
-      done(null, aws);
-    },
-    stream(done) {
-      if (typeof input.on === 'function') {
-        return done(null, input);
-      }
-      if (Buffer.isBuffer(input)) {
-        return done(null, input);
-      }
-      const stream = fs.createReadStream(input);
-      done(null, stream);
-    },
-    filename(stream, done) {
-      const filename = options.filename || options.path || stream.path;
-      if (!filename) {
-        return done(new Error('must pass in path if a buffer'));
-      }
-      done(null, filename);
-    },
-    fileKey(filename, done) {
-      let fileKey = path.basename(filename);
-      if (options.noprefix !== true) {
-        fileKey = `${datefmt('%Y-%m-%d', new Date())}/${(+new Date)}/${fileKey}`;
-      }
-      if (options.folder) {
-        fileKey = path.join(options.folder, fileKey);
-      }
-      done(null, fileKey);
-    },
-    s3Options(filename, fileKey, stream, done) {
-      const s3Options = {
-        Bucket: options.bucket,
-        Key: fileKey,
-        Body: stream,
-        ACL: options.public ? 'public-read' : 'private',
-        ContentType: mimeLookup(filename),
-      };
-      if (options.maxAge) {
-        s3Options.CacheControl = `max-age=${options.maxAge}`;
-      }
-      if (s3Options.acl) {
-        s3Options.ACL = options.acl;
-      }
-      done(null, s3Options);
-    },
-    upload(aws, s3Options, done) {
-      aws.upload(s3Options, done);
-    },
-    location(upload, fileKey, done) {
-      if (!options.host) {
-        return done();
-      }
-      upload.Location = url.resolve(options.host, fileKey);
-      done();
-    }
-  }, (err, results) => {
-    if (err) {
-      return allDone(err);
-    }
-    allDone(null, results.upload);
-  });
+  const aws = awsAuth(AWS, 'S3', options);
+  const stream = (typeof input.on === 'function' || Buffer.isBuffer(input)) ? input : fs.createReadStream(input);
+  const filename = options.filename || options.path || stream.path;
+  if (!filename) {
+    throw new Error('must pass in path if a buffer');
+  }
+  let fileKey = path.basename(filename);
+  if (options.noprefix !== true) {
+    fileKey = `${datefmt('%Y-%m-%d', new Date())}/${(+new Date)}/${fileKey}`;
+  }
+  if (options.folder) {
+    fileKey = path.join(options.folder, fileKey);
+  }
+  const s3Options = {
+    Bucket: options.bucket,
+    Key: fileKey,
+    Body: stream,
+    ACL: options.public ? 'public-read' : 'private',
+    ContentType: mimeLookup(filename),
+  };
+  if (options.maxAge) {
+    s3Options.CacheControl = `max-age=${options.maxAge}`;
+  }
+  if (s3Options.acl) {
+    s3Options.ACL = options.acl;
+  }
+  const upload = await util.promisify(aws.upload)(s3Options);
+  if (!options.host) {
+    return;
+  }
+  upload.Location = url.resolve(options.host, fileKey);
+  return upload;
 };
--- test/lib.test.js
+++ test/lib.test.js
@@ -1,5 +1,4 @@
 /* global describe, it */
-const chai = require('chai');
 const s3put = require('../main.js');
 const fs = require('fs');
 const path = require('path');
@@ -9,152 +8,115 @@ const testImage = path.join(__dirname, testImageBase);
 const testFileBase = 'snoopy.txt';
 const testFile = path.join(__dirname, testFileBase);
 const datefmt = require('datefmt');
+const tap = require('tap');
 
-describe('can be used as a library', () => {
-  if (process.env.AWS_BUCKET === undefined) {
-    throw new Error('You must define AWS_BUCKET');
+if (process.env.AWS_BUCKET === undefined) {
+  throw new Error('You must define AWS_BUCKET');
+}
+
+tap.test('should be able to upload an unmodified image', async(t) => {
+  const stream = fs.createReadStream(testImage);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE
+  };
+  try {
+    const response = await s3put(stream, options);
+    t.notEqual(response.key.indexOf(testImageBase), -1);
+    t.end();
+  } catch (e) {
+    console.log('threw err')
+    console.log(e)
   }
-  it('should be able to upload an unmodified image', (done) => {
-    const stream = fs.createReadStream(testImage);
-    const options = {
-      bucket: process.env.AWS_BUCKET,
-      profile: process.env.AWS_PROFILE
-    };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(response.key).to.include(testImageBase);
-      done();
-    });
-  });
-  
-  it('should support buffers', (done) => {
-    fs.readFile(testImage, (err, buffer) => {
-      const options = {
-        bucket: process.env.AWS_BUCKET,
-        profile: process.env.AWS_PROFILE,
-        path: testImage
-      };
-      s3put(buffer, options, (err, response) => {
-        if (err) {
-          return done(err);
-        }
-        chai.expect(response.key).to.include(testImageBase);
-        done();
-      });
-    });
+});
 
-  });
-
-  it('should be able to upload a file to folder', (done) => {
-    const stream = fs.createReadStream(testImage);
+tap.test('should support buffers', (t) => {
+  fs.readFile(testImage, async(err, buffer) => {
     const options = {
       bucket: process.env.AWS_BUCKET,
       profile: process.env.AWS_PROFILE,
-      folder: 'folder'
+      path: testImage
     };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(response.key).to.include('folder');
-      done();
-    });
-  });
-
-  it('should be able to upload a non-image', (done) => {
-    const stream = fs.createReadStream(testFile);
-    const options = {
-      bucket: process.env.AWS_BUCKET,
-      profile: process.env.AWS_PROFILE
-    };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(response.key).to.include(testFileBase);
-      done();
-    });
+    const response = await s3put(buffer, options);
+    t.notEqual(response.key.indexOf(testImageBase), -1);
+    t.end();
   });
 });
 
-describe('uses the --prefix option to get the key', () => {
-  it('by default should upload the image to a sub-folder', (done) => {
-    const stream = fs.createReadStream(testImage);
-    const options = {
-      bucket: process.env.AWS_BUCKET,
-      profile: process.env.AWS_PROFILE,
-      noprefix: true
-    };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(response.key).to.not.include(datefmt('%Y-%m-%d', new Date()));
-      done();
-    });
-  });
+tap.test('should be able to upload a file to folder', async(t) => {
+  const stream = fs.createReadStream(testImage);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE,
+    folder: 'folder'
+  };
+  const response = await s3put(stream, options);
+  t.notEqual(response.key.indexOf('folder'), -1);
+  t.end();
 });
 
-describe('uses the --public option to control whether the image hosted on s3 is available to everyone', () => {
-  it('--public option will set the ACL for the image to "public":', (done) => {
-    const stream = fs.createReadStream(testImage);
-    const options = {
-      bucket: process.env.AWS_BUCKET,
-      profile: process.env.AWS_PROFILE,
-      noprefix: true,
-      public: true
-    };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(typeof response.Location).to.equal('string');
-      // now try to get the image from s3 (if it returns anything at all, it was public):
-      wreck.get(response.Location, (err, response, payload) => {
-        chai.expect(err).to.equal(null);
-        chai.expect(response.statusCode).to.equal(200);
-        chai.expect(payload).to.exist;
-        done();
-      });
-    });
-  });
+tap.test('should be able to upload a non-image', async(t) => {
+  const stream = fs.createReadStream(testFile);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE
+  };
+  const response = await s3put(stream, options);
+  t.notEqual(response.key.indexOf(testFileBase), -1);
+  t.end();
 });
 
-describe('host', () => {
+tap.test('uses the --prefix option to get the key, by default should upload the image to a sub-folder', async(t) => {
+  const stream = fs.createReadStream(testImage);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE,
+    noprefix: true
+  };
+  const response = await s3put(stream, options);
+  t.equal(response.key.indexOf(datefmt('%Y-%m-%d', new Date())), -1);
+  t.end();
+});
 
-  it('should override host if set', (done) => {
-    const stream = fs.createReadStream(testImage);
-    const options = {
-      bucket: process.env.AWS_BUCKET,
-      profile: process.env.AWS_PROFILE,
-      noprefix: true,
-      host: 'http://testhost.com',
-    };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(response.Location).to.equal('http://testhost.com/snoopy.jpg');
-      done();
-    });
-  });
-  it('should override host if set with folder', (done) => {
-    const stream = fs.createReadStream(testImage);
-    const options = {
-      bucket: process.env.AWS_BUCKET,
-      profile: process.env.AWS_PROFILE,
-      noprefix: true,
-      folder: 'blah',
-      host: 'http://testhost.com',
-    };
-    s3put(stream, options, (err, response) => {
-      if (err) {
-        return done(err);
-      }
-      chai.expect(response.Location).to.equal('http://testhost.com/blah/snoopy.jpg');
-      done();
-    });
-  });
+tap.test('uses the --public option to control whether the image hosted on s3 is available to everyone --public option will set the ACL for the image to "public":', async(t) => {
+  const stream = fs.createReadStream(testImage);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE,
+    noprefix: true,
+    public: true
+  };
+  const response = await s3put(stream, options);
+  t.equal(typeof response.Location, 'string');
+  // now try to get the image from s3 (if it returns anything at all, it was public):
+  const { res, payload } = await wreck.get(response.Location);
+  t.equal(res.statusCode, 200);
+  t.equal(typeof payload, 'object');
+  t.end();
+});
+
+tap.test('should override host if set', async(t) => {
+  const stream = fs.createReadStream(testImage);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE,
+    noprefix: true,
+    host: 'http://testhost.com',
+  };
+  const response = await s3put(stream, options);
+  t.equal(response.Location, 'http://testhost.com/snoopy.jpg');
+  t.end();
+});
+
+tap.test('should override host if set with folder', async(t) => {
+  const stream = fs.createReadStream(testImage);
+  const options = {
+    bucket: process.env.AWS_BUCKET,
+    profile: process.env.AWS_PROFILE,
+    noprefix: true,
+    folder: 'blah',
+    host: 'http://testhost.com',
+  };
+  const response = await s3put(stream, options);
+  t.equal(response.Location, 'http://testhost.com/blah/snoopy.jpg');
 });
